2aa753a52efb07c305301be05bd3850b89670246
estherpj <e.perezj.2021@alumnos.urjc.es>
2022-03-22 19:27:54 +0100

    Practices

--- P1/ex9.py
+++ P1/ex9.py
@@ -7,8 +7,8 @@ print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 s1 = Seq()
 s1.read_fasta("U5")
 print(f"Sequence: (Length: {s1.len()}"")", s1,
-      "\nBases:",s1.seq_count_base(),
-      "\nRev:",s1.seq_reverse(),
-      "\nComp:",s1.seq_complement())
+      "\n\tBases:",s1.seq_count_base(),
+      "\n\tRev:",s1.seq_reverse(),
+      "\n\tComp:",s1.seq_complement())
 
 
