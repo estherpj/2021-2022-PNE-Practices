9a23ac2af7722336bf52a616b92b5bb83c1a3a4e
estherpj <e.perezj.2021@alumnos.urjc.es>
2022-03-22 13:06:53 +0100

    Practices

--- P1/ex8.py
+++ P1/ex8.py
@@ -1,18 +1,17 @@
 from Seq1 import Seq
 
-s1 = Seq()
-s2 = Seq("ACTGA")
-s3 = Seq("Invalid sequence")
+sequence_list = ["ACTGA","Invalid sequence"]
 
-print(f"Sequence 1: (Length: {s1.len()}"")", s1,
+s1 = Seq()
+print(f"Sequence 0: (Length: {s1.len()}"")",s1,
       "\nBases:",s1.seq_count_base(),
       "\nRev:",s1.seq_reverse(),
       "\nComp:",s1.seq_complement())
-print(f"Sequence 2: (Length: {s2.len()}"")",s2,
+
+for s in sequence_list:
+      s2 = Seq(s)
+      print(f"\nSequence",(sequence_list.index(s) + 1),f": (Length:{s2.len()})",s2,
       "\nBases:",s2.seq_count_base(),
       "\nRev:",s2.seq_reverse(),
       "\nComp:",s2.seq_complement())
-print(f"Sequence 3: (Length: {s3.len()}"")", s3,
-      "\nBases:",s3.seq_count_base(),
-      "\nRev:",s3.seq_reverse(),
-      "\nComp:", s3.seq_complement())
+
