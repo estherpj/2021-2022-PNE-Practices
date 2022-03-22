34182a46f492ab149ff3765ad36379c46bed38b2
estherpj <e.perezj.2021@alumnos.urjc.es>
2022-03-22 13:18:00 +0100

    Practices

--- P1/ex7.py
+++ P1/ex7.py
@@ -1,15 +1,13 @@
 from Seq1 import Seq
 
 s1 = Seq()
-s2 = Seq("ACTGA")
-s3 = Seq("Invalid sequence")
+print(f"Sequence 0: (Length: {s1.len()}"")", s1,
+      "\n\tBases:",s1.seq_count_base(),
+      "\n\tRev:",s1.seq_reverse())
 
-print(f"Sequence 1: (Length: {s1.len()}"")", s1,
-      "\nBases:",s1.seq_count_base(),
-      "\nRev:",s1.seq_reverse())
-print(f"Sequence 2: (Length: {s2.len()}"")",s2,
-      "\nBases:",s2.seq_count_base(),
-      "\nRev:",s2.seq_reverse())
-print(f"Sequence 3: (Length: {s3.len()}"")", s3,
-      "\nBases:",s3.seq_count_base(),
-      "\nRev:",s3.seq_reverse())
+sequence_list = ["ACTGA","Invalid sequence"]
+for s in sequence_list:
+      s2 = Seq(s)
+      print(f"Sequence",(sequence_list.index(s) + 1),f": (Length: {s2.len()}"")",s2,
+      "\n\tBases:",s2.seq_count_base(),
+      "\n\tRev:",s2.seq_reverse())
--- P1/ex8.py
+++ P1/ex8.py
@@ -4,14 +4,14 @@ sequence_list = ["ACTGA","Invalid sequence"]
 
 s1 = Seq()
 print(f"Sequence 0: (Length: {s1.len()}"")",s1,
-      "\nBases:",s1.seq_count_base(),
-      "\nRev:",s1.seq_reverse(),
-      "\nComp:",s1.seq_complement())
+      "\n\tBases:",s1.seq_count_base(),
+      "\n\tRev:",s1.seq_reverse(),
+      "\n\tComp:",s1.seq_complement())
 
 for s in sequence_list:
       s2 = Seq(s)
-      print(f"\nSequence",(sequence_list.index(s) + 1),f": (Length:{s2.len()})",s2,
-      "\nBases:",s2.seq_count_base(),
-      "\nRev:",s2.seq_reverse(),
-      "\nComp:",s2.seq_complement())
+      print(f"Sequence",(sequence_list.index(s) + 1),f": (Length:{s2.len()})",s2,
+      "\n\tBases:",s2.seq_count_base(),
+      "\n\tRev:",s2.seq_reverse(),
+      "\n\tComp:",s2.seq_complement())
 
