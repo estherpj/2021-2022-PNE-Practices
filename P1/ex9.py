39deb59d744b4dc3250247eaca685c1db70daa49
estherpj <e.perezj.2021@alumnos.urjc.es>
2022-03-22 19:37:10 +0100

    Practices

--- P1/ex9.py
+++ P1/ex9.py
@@ -4,11 +4,15 @@ PRACTICE = 1
 EXERCISE = 9
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
-s1 = Seq()
-s1.read_fasta("U5")
-print(f"Sequence: (Length: {s1.len()}"")", s1,
-      "\n\tBases:",s1.seq_count_base(),
-      "\n\tRev:",s1.seq_reverse(),
-      "\n\tComp:",s1.seq_complement())
+try:
+      s1 = Seq()
+      s1.read_fasta("U5")
+      print(f"Sequence: (Length: {s1.len()}"")", s1,
+            "\n\tBases:",s1.seq_count_base(),
+            "\n\tRev:",s1.seq_reverse(),
+            "\n\tComp:",s1.seq_complement())
+
+except FileNotFoundError:
+      print("The file has not been found.")
 
 
