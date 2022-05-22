cb667f2b12ed1b0e884453a00414bd2d211578ad
estherpj <e.perezj.2021@alumnos.urjc.es>
2022-05-23 00:33:24 +0200

    practice 8

--- P8-Final-Project/server.py
+++ P8-Final-Project/server.py
@@ -15,6 +15,10 @@ HTML_FOLDER = "./html/"
 SERVER = 'rest.ensembl.org'
 ARGUMENT = "?content-type=application/json"
 
+genes_dict = {"SRCAP": "ENSG00000080603","FRAT1": "ENSG00000165879","ADA":"ENSG00000196839",
+              "FXN":"ENSG00000165060", "RNU6_269P":"ENSG00000212379","MIR633":"ENSG00000207552",
+              "TTTY4C":"ENSG00000228296","RBMY2YP":"ENSG00000227633","FGFR3":"ENSG00000068078",
+              "KDR":"ENSG00000128052 ","ANK2":"ENSG00000145362"}
 
 def dic_info_server(url):
     conn = http.client.HTTPConnection(SERVER)
@@ -29,10 +33,12 @@ def dic_info_server(url):
     data1 = json.loads(data1)
     return data1
 
+
 def read_html_file(filename):
     contents = pathlib.Path(HTML_FOLDER + filename).read_text()
     contents = j.Template(contents)
     return contents
+
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
@@ -50,18 +56,111 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             contents = read_html_file("index.html").render()
 
         elif path == "/listSpecies":
-            n_species = arguments["n_species"]
-            dictionary_info = dic_info_server("info/species")
-            species_list = dictionary_info["species"]
-            species_list = species_list[0:n_species]
-            contents = read_html_file("html/listSpecies.html").render(context={"species":species_list})
+            try:
+                n_species = int(arguments["n_species"][0])
+                dictionary_info = dic_info_server("info/species")
+                species_info_list = dictionary_info["species"]
+                species_list = []
+                for i in species_info_list:
+                    species_list.append(i["common_name"])
 
-        #elif path == "/karyotype":
+                correct_list_species = []
+                for i in species_list:
+                    if i not in correct_list_species:
+                        correct_list_species.append(i)
+                    else:
+                        pass
+                total_species = len(correct_list_species)
+                species_html = "<br><br>"
+                for n in correct_list_species[0:n_species]:
+                    species_html += "<ul>" + "<li>" + " " + n + "<br>" + "</li>" + "</ul>"
+                contents = read_html_file("listSpecies.html").render(context={"species": species_html,"total_species": total_species,"n_species": n_species})
+
+            except ValueError:
+                contents = pathlib.Path("html/error.html").read_text()
+
+        elif path == "/karyotype":
+            species = arguments["specie"][0]
+            dictionary_info_1 = dic_info_server("info/species")
+            species_info_list = dictionary_info_1["species"]
+            species_name = []
+            for i in species_info_list:
+                species_name.append(i["common_name"])
+            if species not in species_info_list:
+                contents = pathlib.Path("html/error.html").read_text()
+            else:
+                dic_specie = dic_info_server("info/assembly/" + species)
+                karyotype = dic_specie["karyotype"]
+                karyotype_html = "<br><br>"
+                for n in karyotype:
+                    karyotype_html += " " + n + "<br>"
+                contents = read_html_file("karyotype.html").render(context={"karyotype": karyotype_html})
+
+        elif path == "/chromosomeLength":
+            species = arguments["specie"][0]
+            chromosome = arguments["chromo"][0]
+
+            dic_specie = dic_info_server("info/assembly/" + species)
+            list_specie = dic_specie["top_level_region"]
+            length = ""
+            for i in list_specie:
+                if i["name"] == chromosome:
+                    length = i["length"]
+            contents = read_html_file("chromosomeLength.html").render(context={"length": length})
+
+        elif path == "/geneSeq":
+            gene = arguments["gene"][0]
+            dic_specie_gene = dic_info_server("lookup/symbol/homo_sapiens/"+gene)
+            id_gene = dic_specie_gene["id"]
+            dic_gene = dic_info_server("sequence/id/"+id_gene)
+            sequence_gene = dic_gene["seq"]
+            correct_sequence_gene = ""
+            n = 0
+            for i in sequence_gene:
+                correct_sequence_gene += i
+                n += 1
+                if n == 100:
+                    correct_sequence_gene += "<br>"
+                    n = 0
+            contents = read_html_file("gene.html").render(context={"gene": correct_sequence_gene})
+
+        elif path == "/geneInfo":
+            gene = arguments["gene"][0]
+            dic_specie_gene = dic_info_server("lookup/symbol/homo_sapiens/" + gene)
+            start_gene = dic_specie_gene["start"]
+            end_gene = dic_specie_gene["end"]
+            id_gene = dic_specie_gene["id"]
+            length = int(end_gene) - int(start_gene)
+            chromosome_name = dic_specie_gene["seq_region_name"]
+
+            contents = read_html_file("infogene.html").render(context={"start": start_gene, "end": end_gene, "id_gene": id_gene,"chromosome": chromosome_name, "length": length})
+
+        elif path == "/geneCalc":
+            gene = arguments["gene"][0]
+            id_gene = genes_dict[gene]
+            dic_gene = dic_info_server("sequence/id/" + id_gene)
+            sequence_gene = dic_gene["seq"]
+            seq = Seq(sequence_gene)
+            percentage_bases = seq.percentages_bases()
+            bases = "<br><br>"
+            for key, value in percentage_bases.items():
+                bases += key + " : " + str(value[0]) + ", " + str(round(value[1], 2)) + "%" + "<br>"
+                contents = read_html_file("calculusbases.html").render(context={"gene": gene, "calc_bases": bases})
+
+        elif path == "/geneList":
+            chromo = arguments["chromo"][0]
+            start = arguments["start"][0]
+            end = arguments["end"][0]
+            info_chromo = chromo + ":" + start + "-" + end
+            list_chromosome = dic_info_server("/phenotype/region/homo_sapiens/" + info_chromo)
+            genes_found = []
+            for i in list_chromosome:
+                for n in i["phenotype_associations"]:
+                    for e in n:
+                        for c in e["attributes"]:
 
 
-        #elif path == "/chromosomeLength":
-
-
+                    contents = read_html_file("chromo_genes.html").render(context={"chromo": chromo, "start": start, "end": end, "genes_found": genes_found})
         else:
             contents = pathlib.Path("html/error.html").read_text()
 
@@ -72,15 +171,13 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         self.wfile.write(contents.encode())
         return
 
+
 Handler = TestHandler
 
-# -- Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
 
     print("Serving at PORT", PORT)
 
-    # -- Main loop: Attend the client. Whenever there is a new
-    # -- clint, the handler is called
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
